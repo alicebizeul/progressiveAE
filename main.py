a634d0b5af7da90737cee635501a9a560e556985
alicebizeul <alice.bizeul@epfl.ch>
2020-04-21 12:10:53 -0400

    README + debugging

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from options import Opts
 def main(config):
 
     if config.task == 'train':
-        pgvae = PGVAE(latent_size=config.latent_size,generator_folder=config.generator_folder)
+        pgvae = PGVAE(latent_size=config.latent,generator_folder=config.generator_folder)
         pgvae.train(tf_folder=config.tf_folder,start_res = config.start_res, stop_res = config.stop_res,epochs=config.epochs,save_folder=config.save_folder)
 
 if __name__ == '__main__':
