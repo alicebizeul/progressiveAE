49d54f51267f058d3efeba547bcb37bcc142c8b6
alicebizeul <alice.bizeul@epfl.ch>
2020-04-30 11:28:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from options import Opts
 def main(config):
 
     if config.task == 'train':
-        pgvae = PGVAE(latent_size=config.latent,generator_folder=config.generator_folder,config.restore) # make sure strategy is true when multiple GPUs available
+        pgvae = PGVAE(latent_size=config.latent,generator_folder=config.generator_folder,restore=config.restore) # make sure strategy is true when multiple GPUs available
         pgvae.train(start_width=config.start_res, stop_width=config.stop_res,save_folder=config.save_folder,num_samples=config.num_samples)
 
 if __name__ == '__main__':
