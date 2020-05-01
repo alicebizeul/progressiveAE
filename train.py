51d95ab9ddb9477ad9c586d0c4a2e9d4b16ebd27
alicebizeul <alice.bizeul@epfl.ch>
2020-05-01 00:05:03 -0400

    Update train.py

--- train.py
+++ train.py
@@ -73,9 +73,10 @@ class PGVAE:
             checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=self.encoder.train_encoder)
 
             if self.restore and self.current_resolution == 5: 
-                
+                print(self.encoder.train_encoder.get_weights())
                 latest = tf.train.latest_checkpoint(save_folder)
                 checkpoint.restore(latest)
+                print(self.encoder.train_encoder.get_weights())
 
             def train_step(inputs,alpha):
                 with tf.GradientTape() as tape:
