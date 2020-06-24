0cfcf7c729903e84d2f68f0405bc483b0fc5eb9a
alicebizeul <alice.bizeul@epfl.ch>
2020-06-24 09:49:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-#import Vtrain
+import Vtrain
 import train
 from options import Opts
 
@@ -9,9 +9,9 @@ def main(config):
         if not config.variational: 
             pgvae = train.PGVAE(latent_size=config.latent,generator_folder=config.generator_folder,restore=config.restore,param_optimizer=config.param_optimizer) # make sure strategy is true when multiple GPUs available
             pgvae.train(start_width=config.start_res, stop_width=config.stop_res,save_folder=config.save_folder,tf_folder=config.tf_folder,num_samples=config.num_samples)
-        #if config.variational: 
-        #    pgvae = Vtrain.PGVAE(latent_size=config.latent,generator_folder=config.generator_folder,restore=config.restore,param_optimizer=config.param_optimizer) # make sure strategy is true when multiple GPUs available
-        #    pgvae.train(start_width=config.start_res, stop_width=config.stop_res,save_folder=config.save_folder,tf_folder=config.tf_folder,num_samples=config.num_samples)
+        if config.variational: 
+            pgvae = Vtrain.PGVAE(latent_size=config.latent,generator_folder=config.generator_folder,restore=config.restore,param_optimizer=config.param_optimizer) # make sure strategy is true when multiple GPUs available
+            pgvae.train(start_width=config.start_res, stop_width=config.stop_res,save_folder=config.save_folder,tf_folder=config.tf_folder,num_samples=config.num_samples)
 
 if __name__ == '__main__':
     opt = Opts()
