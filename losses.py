9e6b79976c13c07e058b9e03d7e19a4d19daacde
alicebizeul <alice.bizeul@epfl.ch>
2020-06-25 18:08:25 -0400

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,24 +1,22 @@
 import tensorflow as tf
-#import tensorflow_probability as tfp
+import tensorflow_probability as tfp
 import math
 #from keras import backend as K
 
 def Kullback_Leibler(mu,log_sigma):
 
     # or sum over divergence for each normal distribution
-    #q = tfp.distributions.MultivariateNormalDiag(loc=mu,scale_diag=tf.exp(log_sigma),name='Estimate')
-    #p = tfp.distributions.MultivariateNormalDiag(loc=0,scale_diag=1,name='True')
+    q = tfp.distributions.MultivariateNormalDiag(loc=mu,scale_diag=tf.exp(log_sigma),name='Estimate')
+    p = tfp.distributions.MultivariateNormalDiag(loc=0,scale_diag=1,name='True')
 
     # measure the kl divergence over all dimensions and then sum the values
-    #return tf.reduce_sum(tfp.distributions.kl_divergence(p,q,name='KL divergence'),1)
+    return tf.reduce_sum(tfp.distributions.kl_divergence(p,q,name='KL divergence'),1)
     #return tfp.distributions.kl_divergence(p,q,name='KL divergence')
-    return 1
     
 def neg_loglikelihood(true,predict_mu,predict_log_sigma,var_epsilon):
 
     # Gaussian prior, independant pixels
-    loss = ( 0.5 * tf.math.log(predict_log_sigma + var_epsilon)
-            + 0.5 * tf.square(tf.math.subtract(true - predict_mu)) / (predict_log_sigma + var_epsilon))
+    loss = ( 0.5 * tf.math.log(predict_log_sigma + var_epsilon)+ 0.5 * tf.square(tf.math.subtract(true - predict_mu)) / (predict_log_sigma + var_epsilon))
     return tf.math.reduce_sum(loss,axis=[1,2,3])
     
 def Reconstruction_loss(true,predict):
